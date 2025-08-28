18400fdadb2bc2a2115dc4e93cb25705fc260fb3
Jacob Bickerstaff <jacoblukebickerstaff@gmail.com>
2025-08-28 07:09:13 +0100

    [UN-29] import correction

--- services/unacronym_api/src/public_api/db/models/__init__.py
+++ services/unacronym_api/src/public_api/db/models/__init__.py
@@ -1,6 +1,7 @@
 from .acronym_alias import AcronymAlias
 from .base import Base, BaseWithTimestamps
 from .glossary_entry import GlossaryEntry
-from logger import Logger
+from .logger import Logger
+
 
 __all__ = ["Base", "BaseWithTimestamps", "GlossaryEntry", "AcronymAlias", "Logger"]
