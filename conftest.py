6bbed87d0304de670bcb419c6f4ecccf5ff1ffd0
jacob <jacoblukebickerstaff@gmail.com>
2025-09-03 17:04:16 +0100

    [UN-29] remove trio

--- conftest.py
+++ conftest.py
@@ -1,2 +1,10 @@
-# <repo-root>/conftest.py
+import pytest
+
+
 pytest_plugins = ["test_kit.fixtures"]
+
+
+@pytest.fixture
+def anyio_backend():
+    # Force AnyIO tests to run on asyncio so you don't need Trio installed.
+    return "asyncio"
