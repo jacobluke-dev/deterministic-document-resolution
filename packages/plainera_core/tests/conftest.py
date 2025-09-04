4d6fc6407b4179921a18b016e092f5a3c30216d1
jacob <jacoblukebickerstaff@gmail.com>
2025-09-04 13:52:11 +0100

    [UN-29] running ci-local

--- packages/plainera_core/tests/conftest.py
+++ packages/plainera_core/tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+
 from core.domain import DefinitionCandidate
 from core.services.resolver import AcronymResolver
 
--- packages/plainera_core/tests/test_plainera_core/utils/test_utils.py
+++ packages/plainera_core/tests/test_plainera_core/utils/test_utils.py
@@ -1,6 +1,7 @@
 from unittest import mock
 
 import pytest
+
 from utils.utils import get_project_path
 
 
