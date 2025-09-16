4cb5f1d0466d7b47113b50f15531634a02a5cb7e
jacob <jacoblukebickerstaff@gmail.com>
2025-09-16 16:54:00 +0100

    [UN-3]  nlp_helpers.py tested

--- packages/plainera_unacronym/src/plainera_unacronym/nlp/__init__.py
+++ packages/plainera_unacronym/src/plainera_unacronym/nlp/__init__.py
@@ -1,7 +1,6 @@
 from .types import DetectorConfig, DetectorResult, Occurrence, FirstOccurrence
-from .detector import detect_acronyms, Detector
+from .detector import Detector
 
 __all__ = [
-    "DetectorConfig", "DetectorResult", "Occurrence", "FirstOccurrence",
-    "detect_acronyms", "Detector",
+    "DetectorConfig", "DetectorResult", "Occurrence", "FirstOccurrence", "Detector",
 ]
