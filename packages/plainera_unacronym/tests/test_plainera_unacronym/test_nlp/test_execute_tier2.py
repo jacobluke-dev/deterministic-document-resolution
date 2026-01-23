d42820c481f55a48e7bef298f93e9ac3f47a4f37
jacob <jacoblukebickerstaff@gmail.com>
2026-01-23 22:27:56 +0000

    [UN-4] tier2 tests

---
+++ packages/plainera_unacronym/tests/test_plainera_unacronym/test_nlp/test_execute_tier2.py
@@ -0,0 +1,20 @@
+from plainera_unacronym.nlp.execute import detect_and_extract
+from test_plainera_unacronym.test_nlp.common import picked_def
+
+
+class TestDetectAndExtractE2ETier2MixedCaseAcronyms:
+
+    def test_stylised_ios_parenthetical(self):
+        pass
+        # det, extr = detect_and_extract("iOS (iPhone Operating System) is supported.")
+        # assert picked_def(extr, "iOS") in {"iPhone Operating System"}, extr.picks.get("iOS")
+
+    def test_stylised_ebay_parenthetical(self):
+        pass
+        # det, extr = detect_and_extract("eBay (electronic Bay) is a marketplace.")
+        # assert picked_def(extr, "eBay") in {"electronic Bay"}, extr.picks.get("eBay")
+
+    def test_stylised_latex_parenthetical(self):
+        pass
+        # det, extr = detect_and_extract("LaTeX (Lamport TeX) is used for typesetting.")
+        # assert picked_def(extr, "LaTeX") in {"Lamport TeX"}, extr.picks.get("LaTeX")
