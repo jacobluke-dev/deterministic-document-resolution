96ee82f61874deb8143a48c251c9c457a626ed51
jacob <jacoblukebickerstaff@gmail.com>
2026-01-27 21:41:55 +0000

    [UN-4] refactor

--- packages/plainera_unacronym/src/plainera_unacronym/nlp/extraction/__init__.py
+++ packages/plainera_unacronym/src/plainera_unacronym/nlp/extraction/__init__.py
@@ -1,7 +1,6 @@
 # extraction/__init__.py
 from plainera_unacronym.nlp.common.types import ExtractedDefinition
 from plainera_unacronym.nlp.extraction.config import ExtractionConfig
-from plainera_unacronym.nlp.extraction.core.extract_defs import extract_iter, extract_in_text_definitions
 
 
-__all__ = ["ExtractionConfig", "ExtractedDefinition", "extract_iter", "extract_in_text_definitions"]
+__all__ = ["ExtractionConfig", "ExtractedDefinition"]
