3f45db19f86a66f82f70f4fa37d72dcbc3988080
jacob <jacoblukebickerstaff@gmail.com>
2026-02-01 17:57:10 +0000

    [UN-4] added get method

--- packages/plainera_unacronym/src/plainera_unacronym/nlp/plugins/registry.py
+++ packages/plainera_unacronym/src/plainera_unacronym/nlp/plugins/registry.py
@@ -5,3 +5,16 @@ DOMAIN_PLUGINS: dict[str, DomainPlugin] = {}
 
 def register_plugin(p: DomainPlugin) -> None:
     DOMAIN_PLUGINS[p.name] = p
+
+
+def get(names) -> list[DomainPlugin]:
+    """Return registered plugins for the requested names, in order.
+
+    Unknown names are ignored (treat as "not installed").
+    """
+    out: list[DomainPlugin] = []
+    for n in names or ():
+        p = DOMAIN_PLUGINS.get(n)
+        if p is not None:
+            out.append(p)
+    return out
